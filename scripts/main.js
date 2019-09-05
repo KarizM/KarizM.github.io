9bda0d5c24aa1372cfbbaf4f3a274d579ee17522
unknown <mugmuguga@gmail.com>
2019-09-05 17:44:09 +0300

    initial commit

---
+++ scripts/main.js
@@ -0,0 +1,19 @@
+
+function openForm() {
+  document.getElementById("myForm").style.display = "block";
+}
+
+function closeForm() {
+  document.getElementById("myForm").style.display = "none";
+}
+
+var email = document.getElementById("mail");
+
+email.addEventListener("input", function (event) {
+  if (email.validity.typeMismatch) {
+    email.setCustomValidity("I expect an e-mail!");
+  } else {
+    email.setCustomValidity("");
+  }
+});
+
